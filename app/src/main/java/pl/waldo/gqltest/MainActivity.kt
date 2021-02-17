94c534273e0ce387e5fb1b8c43a76011cca801ea
dominiktomczak <]WTxU\f2>
2021-02-17 11:19:02 +0100

    Commit for issue resolution

--- app/src/main/java/pl/waldo/gqltest/MainActivity.kt
+++ app/src/main/java/pl/waldo/gqltest/MainActivity.kt
@@ -1,16 +1,15 @@
 package pl.waldo.gqltest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.rx2.Rx2Apollo
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import queries.GetNotificationsQuery
+import queries.GetAssignedUniformTagsForFacesInFolderQuery
 
 class MainActivity : AppCompatActivity() {
     private var d: Disposable? = null
@@ -20,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val serverUrl = "https://core-graphql.dev.waldo.photos/gql"
-        val accountId = "1N56ZUb6Zp7oiw7nowhbz8r"
-        val authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiYWFhMjUwMzgtYjVjNi00MmFkLWEzYzktZTY5Y2U2Yzg5MDViIiwicm9sZXMiOltdLCJpc3MiOiJ3YWxkbzpjb3JlIiwiZ3JhbnRzIjpbXSwiZXhwIjoxNjE1MTIwNDI5LCJpYXQiOjE2MTI1Mjg0Mjl9.oN45VuGTBF3iIU9Pip0k-ieVPAj674duEaYFzGDBlBQ"
+        val folderId = ""
+        val authToken = ""
 
         val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor { chain ->
@@ -40,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                 .okHttpClient(okHttpClient)
                 .build()
 
-        val q = apolloClient.query(GetNotificationsQuery(accountId, Input.fromNullable(10000), Input.fromNullable(0)))
+        val q = apolloClient.query(GetAssignedUniformTagsForFacesInFolderQuery(folderId))
         val a = Rx2Apollo.from(q)
         this.d = a.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
