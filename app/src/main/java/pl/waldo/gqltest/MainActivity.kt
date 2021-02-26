a5ade81cf22d992678a5335a3abe951b7bde9fea
dominiktomczak <]WTxU\f2>
2021-02-26 17:15:08 +0100

    Notifs problem

--- app/src/main/java/pl/waldo/gqltest/MainActivity.kt
+++ app/src/main/java/pl/waldo/gqltest/MainActivity.kt
@@ -9,7 +9,7 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import queries.GetAssignedUniformTagsForFacesInFolderQuery
+import queries.GetNotificationsQuery
 
 class MainActivity : AppCompatActivity() {
     private var d: Disposable? = null
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val serverUrl = "https://core-graphql.dev.waldo.photos/gql"
-        val folderId = ""
-        val authToken = ""
+        val accountId = "1TPSzRw6kEqn9gyRweX6hbj"
+        val authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiZDVhZmZjMzAtM2ZlOC00OWQzLTlmNGYtMDE2Y2I2OTA2M2M2Iiwicm9sZXMiOltdLCJpc3MiOiJ3YWxkbzpjb3JlIiwiZ3JhbnRzIjpbXSwiZXhwIjoxNjE2Njk0MDk3LCJpYXQiOjE2MTQxMDIwOTd9._zo9bly9YR8O6xQsKMv8PSqsg5QmcsTI3mtHqMUitMI"
 
         val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor { chain ->
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                 .okHttpClient(okHttpClient)
                 .build()
 
-        val q = apolloClient.query(GetAssignedUniformTagsForFacesInFolderQuery(folderId))
+        val q = apolloClient.query(GetNotificationsQuery(accountId))
         val a = Rx2Apollo.from(q)
         this.d = a.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
